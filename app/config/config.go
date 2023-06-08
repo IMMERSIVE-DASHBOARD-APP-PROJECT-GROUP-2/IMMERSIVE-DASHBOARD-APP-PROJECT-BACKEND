7093910798fa8ab3185663f017964390435e10ad
Khadafi15 <c2006g0480@bangkit.academy>
2023-06-08 22:34:50 +0800

    Update config

--- app/config/config.go
+++ app/config/config.go
@@ -1,50 +1,83 @@
 package config
 
 import (
-	"log"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
 
-	"github.com/spf13/viper"
+    "github.com/spf13/viper"
 )
 
 var (
-	SECRET_JWT = ""
+    SECRET_JWT = ""
 )
 
 type AppConfig struct {
-	DB_USERNAME string
-	DB_PASSWORD string
-	DB_HOSTNAME string
-	DB_PORT     int
-	DB_NAME     string
-	jwtKey      string
+    DB_USERNAME string
+    DB_PASSWORD string
+    DB_HOSTNAME string
+    DB_PORT     int
+    DB_NAME     string
+    jwtKey      string
 }
 
-func InitConfig() *AppConfig {
-	return ReadEnv()
+func InitConfig() AppConfig {
+    return ReadEnv()
 }
 
-func ReadEnv() *AppConfig {
+func ReadEnv()AppConfig {
+    // inisialisasi variabel dg type struct AppConfig
+    app := AppConfig{}
+    isRead := true
 
-	app := AppConfig{}
+    // proses mencari & membaca environment var dg key tertentu
+    if val, found := os.LookupEnv("JWT_KEY"); found {
+        app.jwtKey = val
+        isRead = false
+    }
+    if val, found := os.LookupEnv("DBUSER"); found {
+        app.DB_USERNAME = val
+        isRead = false
+    }
+    if val, found := os.LookupEnv("DBPASS"); found {
+        app.DB_PASSWORD = val
+        isRead = false
+    }
+    if val, found := os.LookupEnv("DBHOST"); found {
+        app.DBHOSTNAME = val
+        isRead = false
+    }
+    if val, found := os.LookupEnv("DBPORT"); found {
+        cnv,  := strconv.Atoi(val)
+        app.DB_PORT = cnv
+        isRead = false
+    }
+    if val, found := os.LookupEnv("DBNAME"); found {
+        app.DB_NAME = val
+        isRead = false
+    }
 
-	viper.AddConfigPath(".")
-	viper.SetConfigName("local")
-	viper.SetConfigType("env")
+    if isRead {
+        viper.AddConfigPath(".")
+        viper.SetConfigName("local")
+        viper.SetConfigType("env")
 
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Println("error read config:", err.Error())
-		return nil
-	}
+        err := viper.ReadInConfig()
+        if err != nil {
+            log.Println("error read config : ", err.Error())
+            return nil
+        }
 
-	app.jwtKey = viper.GetString("JWT_KEY")
-	app.DB_USERNAME = viper.GetString("DBUSER")
-	app.DB_PASSWORD = viper.GetString("DBPASS")
-	app.DB_HOSTNAME = viper.GetString("DBHOST")
-	app.DB_PORT = viper.GetInt("DBPORT")
-	app.DB_NAME = viper.GetString("DBNAME")
+        app.jwtKey = viper.Get("JWT_KEY").(string)
+        app.DB_USERNAME = viper.Get("DBUSER").(string)
+        app.DB_PASSWORD = viper.Get("DBPASS").(string)
+        app.DB_HOSTNAME = viper.Get("DBHOST").(string)
+        app.DBPORT,  = strconv.Atoi(viper.Get("DBPORT").(string))
+        app.DB_NAME = viper.Get("DBNAME").(string)
+    }
 
-	SECRET_JWT = app.jwtKey
-
-	return &app
+    SECRET_JWT = app.jwtKey
+    fmt.Println("check", app.jwtKey)
+    return &app
 }
